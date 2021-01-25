2c7f9acd54247f8f79ddc5378cf2ffa5eabc7b19
Wang <810398265@qq.com>
2021-01-25 23:11:21 +0800

    a bug fixed

--- src/components/common/toast/Toast.vue
+++ src/components/common/toast/Toast.vue
@@ -34,8 +34,9 @@
   top: 50%;
   left: 50%;
   transform: translate(-50%,-50%);
-  padding:.3rem .3rem;
+  padding:.4rem;
   color: #fff;
+  font-size: .9rem;
   border-radius: 5px;
   background-color: rgba(0,0,0,.75);
   z-index: 999;
